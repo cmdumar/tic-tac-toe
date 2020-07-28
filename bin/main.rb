b41273740083d375ba7431ce66626e421c8a4477
mohammadumar28 <mohammadumar28@gmail.com>
2020-07-28 18:45:26 +0530

    Add mock playing the game

--- bin/main.rb
+++ bin/main.rb
@@ -44,42 +44,39 @@ class Game
     puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
     puts '---------'
     puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
-    user_inputs_index
+    player_input(@plyr1)
+    player_input(@plyr2)
   end
 
-  def user_inputs_index
-    print "#{@plyr1}'s Turn: "
-    @user_input = gets.chomp
-    # @user_input = @user_input.is_a?(Numeric) ? @user_input : @user_input.to_i
-    if !@user_input.is_a?(Numeric) or (@user_input.to_i < 0 or @user_input.to_i > 10)
-        print "Input number between 1 and 9: "
-        @user_input = gets.chomp
-    end    
-    print "#{@plyr2}'s Turn: "
-    @user_input = gets.chomp
-    #  @user_input = @user_input.is_a?(Numeric) ? @user_input : @user_input.to_i
-    if !@user_input.is_a?(Numeric) or (@user_input.to_i < 0 or @user_input.to_i > 10)
-        print "Input number between 1 and 9: "
-        @user_input = gets.chomp
+  def player_input(player)
+    print "#{player}'s Turn: "
+    @user_input = gets.chomp.to_i
+
+    get_num = true
+
+    while get_num
+      if @user_input <= 0 or @user_input > 9
+        print "#{player}, Pls give a number between 1 and 9: "
+        @user_input = gets.chomp.to_i
+      end
+      @user_input.positive? && @user_input < 10 ? get_num = false : nil
     end
-    @game_over = false
   end
 
   def playing
-     game_on = true
-     i = 0
-     while game_on
-        display_board
-        if i == 4
-            puts "Game is over"
-            game_on = false   
-        end
-        i += 1
-     end       
+    game_on = true
+    i = 0
+    while game_on
+      display_board
+      if i == 4
+          puts "Game is over"
+          game_on = false
+      end
+      i += 1
+    end
   end
 end
 
 game = Game.new
 game.start_game
 game.playing
-
