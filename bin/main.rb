5e15720f6e11632b8567d3f0d6e0610dfcc5e8d3
mohammadumar28 <mohammadumar28@gmail.com>
2020-07-29 19:44:32 +0530

    Complete Game Logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,50 +1,5 @@
 #!/usr/bin/env ruby
-
-# Game Procedure
-# =================================================================
-# Methods and classes for Game.
-# Two players needed to play game.
-# We need to initialize a game board
-# We need winning combinations
-# Get inputs from the user
-# Get user tokens as "X" and "Y"
-# Each user should take a turn
-# Check turn count
-# check for valid turns of users
-# User should be able to win games
-# User should be able to draw games
-# User should get notification for game over.
-
-# WINNING_COMBINATIONS = [
-#   [0, 1, 2],
-#   [3, 4, 5],
-#   [6, 7, 8],
-#   [0, 3, 6],
-#   [1, 4, 7],
-#   [2, 5, 8],
-#   [0, 4, 8],
-#   [2, 4, 6]
-# ]
 require_relative '../lib/game'
-
-user = Game.new
-
-def take_turn
-  puts "Please choose a number between 1 and 9:"
-  
-  user_input = gets.chomp.strip
-  # convert user_input to index
-  index = user.player_input_to_index(user_input)
-  if user.valid_move?(index)
-    player_id = current_player
-    user.make_move(index, player_id) #match current player to index
-    user.display_board
-    p "Player " + user.make_move(index, player_id) + " Just played!"
-  else
-    take_turn
-  end
-end
 
 game = Game.new
 game.start_game
-# game.playing
--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,7 @@
-class Game
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Style/MultipleComparison
 
+class Game
   WINNING_COMBINATIONS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -9,159 +11,126 @@ class Game
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-]
+  ].freeze
 
-# def initialize
-#     @board = [*1..9]
-#   end
+  def initialize
+    @board = [*1..9]
+  end
 
-#   def start_game
-#     print 'Player 1: '
-#     @plyr1 = gets.chomp
-#     print 'Player 2: '
-#     @plyr2 = gets.chomp
-#     # validate player input
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts '-----------'
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts '-----------'
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
-# end
+  def player_input_to_index(user_input)
+    user_input.to_i - 1
+  end
 
-def initialize
-  @board = Array.new(9, " ")
-end
+  def position_taken?(index)
+    !(@board[index].nil? || (@board[index].to_i.positive? && @board[index].to_i < 10))
+  end
 
-def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
+  def valid_move?(index)
+    index.between?(0, 8) && !position_taken?(index)
+  end
 
-# Whatever a user chooses should be converted to int and returns an index
-def player_input_to_index(user_input)
-  user_input.to_i - 1
-end
+  def make_move(index, current_player = 'X')
+    @board[index] = current_player
+  end
 
-
-
-def position_taken?(index)
-  # It should check if position is empty or taken
-  !(@board[index].nil? || @board[index] == " ")
-end
-
-def valid_move?(index)
-  # Validity should check if index chosen is b/w 0 and 8 and position not taken for that index
-  index.between?(0,8) && !position_taken?(index)
-end
-
-def make_move(index, current_player = "X")
-  # current_player set to "X"
-  # @board[0] == 1
-  # 1 should be converted to player_id "X" or "Y"
-  @board[index] = current_player
-end
-
-def turn_count
-  counter = 0
-  @board.each do |i|
-    if i == "X" || i == "Y"
-      counter += 1
+  def turn_count
+    counter = 0
+    @board.each do |i|
+      counter += 1 if i == 'X' || i == 'Y'
     end
+    counter
   end
-  return counter
-end
 
-def current_player
-  #if the turn count is an even number, that means Y just played since it checks for X first, 
-  # so the next/current player is X
-  num_turns = turn_count
-  if num_turns % 2 == 0
-    player = "X"
-  else
-    player = "Y"
+  def current_player
+    num_turns = turn_count
+    player = if num_turns.even?
+               'X'
+             else
+               'Y'
+             end
+    player
   end
-  return player
-end
 
-# def take_turn
-#   puts "Please choose a number between 1 and 9:"
-  
-#   user_input = gets.chomp.strip
-#   # convert user_input to index
-#   index = player_input_to_index(user_input)
-#   if valid_move?(index)
-#     player_id = current_player
-#     make_move(index, player_id) #match current player to index
-#     display_board
-#     p "Player " + make_move(index, player_id) + " Just played!"
-#   else
-#     take_turn
-#   end
-# end
+  def check_winner?
+    WINNING_COMBINATIONS.each do |combination|
+      spot1 = @board[combination[0]]
+      spot2 = @board[combination[1]]
+      spot3 = @board[combination[2]]
 
+      return combination if spot1 == 'X' and spot2 == 'X' and spot3 == 'X'
 
-def check_winner?
-  WINNING_COMBINATIONS.each {|combination|
-    
-    spot_1 = @board[combination[0]]   #xxx  check_winner[xxx,yyy,false]
-    spot_2 = @board[combination[1]]   #yyy
-    spot_3 = @board[combination[2]]
-
-    if spot_1 == "X" and spot_2 == "X" and spot_3 == "X"
-      return combination
-    elsif spot_1 == "Y" and spot_2 == "Y" and spot_3 == "Y"
-      return combination
+      return combination if spot1 == 'Y' and spot2 == 'Y' and spot3 == 'Y'
     end
-  }
-  return false
-end
-
-def board_full?
-  @board.all? {|i| i == "X" || i == "Y"}
-end
-
-def draw?
-  if !check_winner? && board_full?
-    return true
-  else
-    return false
+    false
   end
-end
 
-def game_over?
-  if check_winner? || draw?
-    return true
-  else
-    return false
+  def board_full?
+    @board.all? { |i| i.is_a?(String) }
   end
-end
 
-def winner
-  winning_combinatn_arr = []
-  winning_combinatn_arr = check_winner? # xxx or yyy or false
-  if winning_combinatn_arr == false
-    return nil
-  else
-    if @board[winning_combinatn_arr[0]] == "X" #check if one of the particular comb is "X"
-      return "X"
+  def draw?
+    if !check_winner? && board_full?
+      true
     else
-      return "Y"
+      false
     end
   end
-end
 
-def start_game
-  while game_over? == false
-    take_turn
+  def game_over?
+    if check_winner? || draw?
+      true
+    else
+      false
+    end
   end
 
-  if check_winner?
-    puts "Hurray! You won, player #{winner}!"
-  elsif draw?
-    puts "You both draw the game!"
+  def winner
+    winning_combinatn_arr = check_winner?
+    nil if winning_combinatn_arr == false
+
+    if @board[winning_combinatn_arr[0]] == 'X'
+      'X'
+    else
+      'Y'
+    end
+  end
+
+  def take_turn
+    display_board
+    print 'Please enter a number between 1 and 9: '
+    user_input = gets.chomp.strip.to_i
+    index = player_input_to_index(user_input)
+    if valid_move?(index)
+      player_id = current_player
+      make_move(index, player_id)
+      puts "Player #{make_move(index, player_id)} Just played!"
+    else
+      take_turn
+    end
+  end
+
+  def start_game
+    take_turn while game_over? == false
+
+    if check_winner?
+      puts "Hurray! You won, player #{winner}!"
+    elsif draw?
+      puts 'You both draw the game!'
+    end
+  end
+
+  def start
+    Gameable.start_game
   end
 end
 
-end
-
-# player_1 = Game.new
-# puts player_1.start_game
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Style/MultipleComparison
