a76d156c5da40f6ab431cad2504c372de56fb160
mohammadumar28 <mohammadumar28@gmail.com>
2020-08-07 21:39:48 +0530

    Complete refactor of the main.rb file and add messages module

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
 require_relative '../lib/game'
 require_relative '../lib/player'
+require_relative '../lib/messages'
 
 class Main
+  include Messages
   attr_accessor :board
   def initialize
     @board = [*1..9]
@@ -10,27 +12,20 @@ class Main
 
   def start_game
     @start = Game.new(@board)
-    getPlayer
+    player_names
     game_loop
   end
 
-  def displayBoard
-    puts "             #{@board[0]} | #{@board[1]} | #{@board[2]}"
-    puts '             --|---|--'
-    puts "             #{@board[3]} | #{@board[4]} | #{@board[5]}"
-    puts '             --|---|--'
-    puts "             #{@board[6]} | #{@board[7]} | #{@board[8]}"
-  end
-
-  def getPlayer
+  def player_names
+    new_line
     print 'Player 1, Type your name: '
     player1 = gets.chomp.strip
-    puts ''
+    new_line
     print 'Player 2, Type your name: '
     player2 = gets.chomp.strip
 
-    player1 = player1.length == 0 ? 'Player - 1' : player1
-    player2 = player2.length == 0 ? 'Player - 2' : player2
+    player1 = player1.length.zero? ? 'Player - 1' : player1
+    player2 = player2.length.zero? ? 'Player - 2' : player2
 
     @player = Player.new(player1, player2)
   end
@@ -47,7 +42,7 @@ class Main
         break
       else
         new_line
-        print 'Invalid Input => Please type a number between 1 and 9: '
+        Messages.invalid_input
         @player_input = gets.chomp.to_i
         new_line
       end
@@ -56,40 +51,38 @@ class Main
 
   def whos_turn
     if @i.even?
-      print "#{@player.player1} - X, Please type a number between 1 and 9: "
+      Messages.input_msg(@player.player1, 'X')
     else
-      print "#{@player.player2} - O, Please type a number between 1 and 9: "
+      Messages.input_msg(@player.player2, 'O')
     end
   end
 
   def game_won
-    if @start.check_winner?
-      if @start.winner == 'X'
-        puts "Hurray! #{@player.player1} won the game!"
-      else
-        puts "Hurray! #{@player.player2} won the game!"
-      end
-      play_again
-      return true
+    return false unless @start.check_winner?
+
+    new_line
+    if @start.winner == 'X'
+      Messages.who_won(@player.player1)
+    else
+      Messages.who_won(@player.player2)
     end
+    new_line
+    play_again
+    true
   end
 
   def game_draw
-    if @start.draw?
-      puts 'The Game is Draw!'
-      play_again
-      true
-    end
+    return false unless @start.draw?
+
+    Messages.draw_msg
+    play_again
+    true
   end
 
   def play_again
-    puts 'Do you want to play again?'
-    new_line
-    puts 'Input Y to play again'
-    new_line
-    puts 'Press any key to exit'
+    Messages.play_again_msgs
     yes = gets.chomp.strip.upcase
-    if yes == 'Y' || yes == 'YES'
+    if yes.include?('Y')
       @board = [*1..9]
       start_game
     else
@@ -98,9 +91,7 @@ class Main
   end
 
   def game_loop
-    new_line
-    displayBoard
-    new_line(2)
+    Messages.display_board(@board)
     @i = 0
     loop do
       break if game_won
@@ -110,9 +101,7 @@ class Main
       whos_turn
       @player_input = gets.chomp.to_i
       input_validator
-      new_line(2)
-      displayBoard
-      new_line(2)
+      Messages.display_board(@board)
     end
   end
 
